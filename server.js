13a9b4f2b976540fe6551b17dc171d8877751bde
Abdessamad <a.aithamou1046@uca.ac.ma>
2024-03-13 06:49:19 +0000

    Initialized docker training

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+let userTask = 'Read Docker Section!';
+
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.send(`
+    <html>
+      <head>
+        <link rel="stylesheet" href="styles.css">
+      </head>
+      <body>
+        <section>
+          <h2>My Tasks</h2>
+          <h3>${userGoal}</h3>
+        </section>
+        <form action="/store-task" method="POST">
+          <div class="form-control">
+            <label>Today Task</label>
+            <input type="text" name="task">
+          </div>
+          <button>Set Today Task</button>
+        </form>
+      </body>
+    </html>
+  `);
+});
+
+app.post('/store-task', (req, res) => {
+  const enteredTask = req.body.task;
+  console.log(enteredGoal);
+  userTask = enteredGoal;
+  res.redirect('/');
+});
+
+app.listen(80);
